87d7bdf213ebf50637926f47ce67ac7b4ff7b518
Yosef Hershberg <berioo2000@gmail.com>
2024-02-04 12:23:34 +0200

    cors origin 8

--- backend/app.js
+++ backend/app.js
@@ -11,10 +11,10 @@ const app = express()
 require('dotenv').config()
 const multer = require('multer');
 
-// app.use(cors({
-//     credentials: 'https://places-app-client.vercel.app',
-//     origin: true,
-// }))
+app.use(cors({
+    credentials: true,
+    origin: 'https://places-app-client.vercel.app',
+}))
 
 app.use(bodyParser.json())
 
